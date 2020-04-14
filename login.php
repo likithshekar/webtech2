e03d531e0c46e731d5f7906fd48608f761fdcbcc
likithshekar <likithshekar01@gmail.com>
2020-04-14 13:28:27 +0530

    Commit

--- login.php
+++ login.php
@@ -1,20 +1,23 @@
-<?php 
-if (isset($_POST['inputusername1']) and isset($_POST['inputpassword1'])){
-$name = $_POST['inputusername1'];
-$pass = $_POST['inputpassword1'];
+<?php
+    session_start();
+    if(isset($_POST['inputusername1']) and isset($_POST['inputpassword1']))
+    {
+        $username = $_POST['inputusername1'];
+        $pass = $_POST['inputpassword1'];
+        $query = "SELECT * FROM `person` WHERE name='$username' and pass='$pass'";
+        $result = mysql_query($query) or die(mysql_error());
+        $count = mysql_num_rows($result);
+        if ($count == 1){
+            $_SESSION['inputusername1'] = $username;
+            header('Location: content.php');
+        }
+        else
+        {
+            $msg = "Wrong credentials";
+        }
+    }
 
-$query = "SELECT * FROM `person` WHERE name='$name' and pass='$pass'";
-$result = mysql_query($query) or die(mysql_error());
-$count = mysql_num_rows($result);
-if ($count == 1){
-$_SESSION['inputusername1'] = $username;
-}else{
-echo "Login failed.";
-}
-}
-if (isset($_SESSION['inputusername1'])){
-$username = $_SESSION['inputusername1'];
-echo "Hello" . $username . "
-";
-echo "<a href='logout.php'>Logout</a>";}
-?>
+    if(isset($msg) & !empty($msg)){
+        echo $msg;
+    }
+ ?>
