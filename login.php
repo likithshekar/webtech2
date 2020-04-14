8996dd19852abb0243409108ed10ac34228bc231
likithshekar <likithshekar01@gmail.com>
2020-04-14 14:29:24 +0530

    Commit

--- login.php
+++ login.php
@@ -1,23 +1,28 @@
 <?php
-    session_start();
-    if(isset($_POST['inputusername1']) and isset($_POST['inputpassword1']))
-    {
-        $username = $_POST['inputusername1'];
-        $pass = $_POST['inputpassword1'];
-        $query = "SELECT * FROM `person` WHERE name='$username' and pass='$pass'";
-        $result = mysql_query($query) or die(mysql_error());
-        $count = mysql_num_rows($result);
-        if ($count == 1){
-            $_SESSION['inputusername1'] = $username;
-            header('Location: content.php');
-        }
-        else
-        {
-            $msg = "Wrong credentials";
-        }
-    }
+session_start();
+if (isset($_POST['inputusername1']) and isset($_POST['inputpassword1'])) {
+    $username = $_POST['inputusername1'];
+    $pass = $_POST['inputpassword1'];
 
-    if(isset($msg) & !empty($msg)){
-        echo $msg;
+    $host = "localhost";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbname = "pharma";
+    // Create connection
+    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+    $query = "SELECT * FROM `login` WHERE username='$username' and pass='$pass'";
+    $result = mysql_query($query) or die(mysql_error());
+    $count = mysql_num_rows($result);
+    if ($count == 1) {
+        $_SESSION['username'] = $username;
+        header('Location: http://localhost/webtech2/home.html');
+    } else {
+        $msg = "Wrong credentials";
     }
- ?>
+}
+
+if (isset($msg) & !empty($msg)) {
+    echo $msg;
+}
+?>
