73efa63f984f838796a399a69f721659b856f353
likithshekar <likithshekar01@gmail.com>
2020-04-15 19:08:17 +0530

    Commit

--- getDetail.php
+++ getDetail.php
@@ -1,19 +1,34 @@
 <?php
 	$text = "AAAAA";
 	if ($_GET["id"] == 0){
-		$text = "Nature";
+		$text = "Crocin";
 	}
 	else if ($_GET["id"] == 1){
-		$text = "Hill";
+		$text = "Crocin - Fast Relief";
 	}
 	else if ($_GET["id"] == 2){
-		$text = "Lake";
+		$text = "Otrivin";
 	}
 	else if ($_GET["id"] == 3){
-		$text = "Sunset";
+		$text = "Nasivion";
 	}
 	elseif ($_GET["id"] == 4){
-		$text = "Nature";
+		$text = "PAN-D";
+	}
+	elseif ($_GET["id"] == 5){
+		$text = "Rantac";
+	}
+	elseif ($_GET["id"] == 6){
+		$text = "Fluticon - FT";
+	}
+	elseif ($_GET["id"] == 7){
+		$text = "Volini";
+	}
+	elseif ($_GET["id"] == 8){
+		$text = "Volini Cream";
+	}
+	else {
+		$text = "Ketosol - 2%";
 	}
     echo $text;
 ?>
--- getImage.php
+++ getImage.php
@@ -12,8 +12,23 @@
 	else if ($_POST["id"] == 3){
 		$fname = "img/image4.jpg";
 	}
-	else{
+	else if ($_POST["id"] == 4){
 		$fname = "img/image5.jpg";
+	}
+	else if ($_POST["id"] == 5){
+		$fname = "img/image6.jpg";
+	}
+	else if ($_POST["id"] == 6){
+		$fname = "img/image7.jpg";
+	}
+	else if ($_POST["id"] == 7){
+		$fname = "img/image8.jpg";
+	}
+	else if ($_POST["id"] == 8){
+		$fname = "img/image9.jpg";
+	}
+	else {
+		$fname = "img/image10.jpg";
 	}
     $file = fopen($fname,"rb");
     $size = filesize($fname);
